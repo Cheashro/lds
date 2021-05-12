f0a2f4472f3f33bd54062453a81a3cbf08f4dc53
Cheashro <cheashro@gmail.com>
2021-05-12 20:55:22 +0800

    Switch bootstrap theme to CoreUI

---
+++ lds/apps/coreui/templatetags/coreui.py
@@ -0,0 +1,57 @@
+
+from django import template
+from django.template.loader import get_template
+
+register = template.Library()
+
+
+@register.filter
+def coreui(element):
+    """
+    Django form render with coreui
+    """
+
+    element_type = element.__class__.__name__.lower()
+    if element_type == 'boundfield':
+        add_input_classes(element)
+
+        template = get_template('coreui/field.html')
+        context = {'field': element}
+    else:
+        has_management = getattr(element, 'management_form', None)
+        if not has_management:
+            for field in element.visible_fields():
+                add_input_classes(field)
+
+            template = get_template('coreui/form.html')
+            context = {'form': element}
+
+    return template.render(context)
+
+
+def add_input_classes(field):
+    """
+    依照不同的 field widget 加上相對應的 class attribute
+    """
+
+    field_classes = field.field.widget.attrs.get('class', '')
+
+    if 'form-control' not in field_classes:
+        field_classes = f'{field_classes} form-control'
+
+    field.field.widget.attrs['class'] = field_classes.strip()
+
+
+@register.filter
+def is_input_prepend_icon(field):
+    return 'input_prepend_icon' in field.field.widget.attrs
+
+
+@register.filter
+def is_input_group(field):
+    return is_input_prepend_icon(field)
+
+
+@register.filter
+def is_input_group_prepend(field):
+    return is_input_prepend_icon(field)
--- lds/lds/settings.py
+++ lds/lds/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
-    'apps.bootstrap',
+    'apps.coreui',
     'apps.lds',
 ]
 
